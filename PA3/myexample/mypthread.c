a4d37523073d4d368a346b8fc2361f8c23b9e5e4
sungbin <21700613@handong.edu>
2019-05-25 07:16:06 +0000

    myexampel1

--- PA3/myexample/mypthread.c
+++ PA3/myexample/mypthread.c
@@ -11,7 +11,7 @@ pthread_mutex_lock(pthread_mutex_t *mutex) {
 	void *(*lockp)(pthread_mutex_t *mutex);
 	lockp = dlsym(RTLD_NEXT, "pthread_mutex_lock");
 	
-	fputs(stderr, "my lock!\n",strerr);
+	fputs("my lock!\n",stderr);
 
 	lockp(mutex);
 
@@ -23,6 +23,6 @@ pthread_mutex_unlock(pthread_mutex_t *mutex) {
 	void *(*unlockp)(pthread_mutex_t *mutex);
 	unlockp = dlsym(RTLD_NEXT, "pthread_mutex_unlock");
 
-
+	fputs("my unlock!!\n",stderr);
 	unlockp(mutex);
 }
