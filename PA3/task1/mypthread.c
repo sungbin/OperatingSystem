f4125531006189019faf6f1af72e5155a10c680b
sungbin <21700613@handong.edu>
2019-05-30 06:07:09 +0900

    I DID IT!

--- PA3/task1/mypthread.c
+++ PA3/task1/mypthread.c
@@ -26,9 +26,9 @@ int edge_count = 0;
 int cyclic();
 int _cyclic(long start, long end);
 long find(long start);
-void mremove(long start, long end);
-Thread find_thread(long tid);
-void draw(Thread thread,long mid);
+void mremove(long m,Thread* thread);
+Thread* find_thread(long tid);
+void draw(Thread* thread,long mid);
 void add_edge(long start, long end);
 
 int
@@ -40,7 +40,7 @@ pthread_mutex_lock(pthread_mutex_t *mutex) {
 	long tid = pthread_self();
 	long mid = (long) mutex;
 
-	Thread thread = find_thread(tid);
+	Thread* thread = find_thread(tid);
 	draw(thread,mid);
 	
 
@@ -49,7 +49,6 @@ pthread_mutex_lock(pthread_mutex_t *mutex) {
                 exit(1);
         }
 	lockp(mutex);
-
 }
 
 int
@@ -61,7 +60,8 @@ pthread_mutex_unlock(pthread_mutex_t *mutex) {
 	long tid = pthread_self();
 	long mid = (long) mutex;
 
-	mremove(mid,tid); //revered data
+	Thread* thread = find_thread(tid);
+	mremove(mid,thread); //revered data
 	unlockp(mutex);
 }
 
@@ -72,13 +72,6 @@ int cyclic() {
 	return sum;
 } 
 int _cyclic(long start, long end) {
-/* for check
-	for(int i = 0; i<count; i++) {
-		char buf[50];
-		sprintf(buf,"start:%ld, end:%ld\n",edges[i].start,edges[i].end);
-		fputs(buf,stderr);
-	}
-*/
 	long in = start;
 	long temp = end;
 	while(temp != -1 && temp != in) {
@@ -98,42 +91,51 @@ long find(long start) {
 	}
 	return -1; //NULL
 }
-void mremove(long start, long end) {
+void mremove(long m,Thread* thread) {
 	int i;
+	for(i = 0; i<thread->mutex_count;i++) {
+		if(thread->mutexs[i] == m) {
+			int j;
+                        for(j = i+1; j<thread->mutex_count; j++) {
+                                thread->mutexs[j-1] = thread->mutexs[j];
+                        }
+                        i--;
+                        thread->mutex_count--;
+		}
+
+	}
 	for(i = 0; i<edge_count; i++) {
-		if(edges[i].start == start && edges[i].end == end) {
-		
+		if(edges[i].start == m || edges[i].end == m) {
+//			fprintf(stderr,"remove: %ld -> %ld\n",edges[i].start,edges[i].end);
 			int j;
 			for(j = i+1; j<edge_count; j++) {
 				edges[j-1] = edges[j];
 			}
+			i--;
 			edge_count--;
-			return;
 		}
 	}
-
 }
-Thread find_thread(long tid) {
+Thread* find_thread(long tid) {
 	int i;
 	for(i = 0; i<thread_count; i++) {
 		if(threads[i].id == tid)
-			return threads[i];
+			return &threads[i];
 	}
 	Thread t = {(long)pthread_self(),0,0x0};
 	threads[thread_count] = t;
 	thread_count++;
-	return t;
+	return &threads[thread_count-1];
 }
-void draw(Thread thread,long mid) {
-	fprintf(stderr,"draw %ld, %d, %d\n",thread.id,thread.mutex_count,edge_count);
+void draw(Thread* thread,long mid) {
 	int i;
-	for(i = 0; i<thread.mutex_count; i++) { // make edges
+	for(i = 0; i<thread->mutex_count; i++) { // make edges
 		int j;
 		int check = 0;
         	for(j = 0; j<edge_count; j++) {
 			Edge edge = edges[j];
 		
-			long start = thread.mutexs[i];
+			long start = thread->mutexs[i];
 			long end = mid;
 			
                 	if(edge.start == start && edge.end == end) {
@@ -141,14 +143,14 @@ void draw(Thread thread,long mid) {
                         	break;
 			}
         	}
-		if(check) continue;
-		add_edge(thread.mutexs[i],mid);
+		if(check || thread->mutexs[i] == mid) continue;
+		add_edge(thread->mutexs[i],mid);
 	}
-	thread.mutexs[thread.mutex_count] = mid;
-	thread.mutex_count++;
+	thread->mutexs[thread->mutex_count] = mid;
+	thread->mutex_count++;
 }
 void add_edge(long start, long end) {
-	fprintf(stderr,"add edge\n");
+//	fprintf(stderr,"add edge: %ld -> %ld, %\d\n",start,end,pthread_self());
 	Edge edge = {start, end};
 	edges[edge_count] = edge;
 	edge_count++;
--- PA3/task1/run.sh
+++ PA3/task1/run.sh
@@ -1,4 +1,4 @@
-# gcc -pthread dinning_deadlock.c -o exec.out
-gcc -pthread abba.c -o exec.out
+gcc -pthread dinning_deadlock.c -o exec.out
+# gcc -pthread abba.c -o exec.out
 gcc -shared -fPIC -o mypthread.so mypthread.c -ldl
 LD_PRELOAD=./mypthread.so ./exec.out
